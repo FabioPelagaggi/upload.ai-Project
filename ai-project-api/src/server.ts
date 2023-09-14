3309f3b8a6dd7fc660cd291bb2ad89359874f60e
Fabio Pelagaggi <fabio.pnunes@al.infnet.edu.br>
2023-09-14 13:36:39 -0300

    v0.5

--- ai-project-api/src/server.ts
+++ ai-project-api/src/server.ts
@@ -2,12 +2,20 @@ import { fastify } from "fastify";
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAICompletionRoute } from "./routes/generate-ai-completion";
+import fastifyCors from "@fastify/cors";
 
 const server = fastify();
+
+server.register(fastifyCors, {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+})
 
 server.register(getAllPromptsRoute);
 server.register(uploadVideoRoute);
 server.register(createTranscriptionRoute);
+server.register(generateAICompletionRoute);
 
 server
   .listen({
